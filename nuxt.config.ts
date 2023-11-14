ff56e53ce3fdf8d4f81a690818002f64e65faf66
Arlen Peiffer <arlenpeiffer@gmail.com>
2023-11-14 14:36:16 -0800

    add unocss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ['@unocss/reset/tailwind.css'],
   devtools: { enabled: true },
   modules: [
+    '@unocss/nuxt',
     '@vueuse/nuxt',
   ]
 })
